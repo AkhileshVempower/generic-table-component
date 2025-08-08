ca32892d952f50686d7ac42979f816531042d4ab
Akhilesh Pratap <akhilesh@vempower.org>
2025-08-08 16:50:59 +0530

    Add delete button

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+
+
+function DeleteButton() {
+  return (
+   <Button variant="danger" size='sm' className='rounded-5 my-3'>Delete Selected</Button>
+  )
+}
+
+export default DeleteButton
